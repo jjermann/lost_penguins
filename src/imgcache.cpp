b0128ba4bb831c202e8a6c9172308365a6c92db3
Jonas Jermann <jjermann2@gmail.com>
2005-03-14 23:44:55 +0000

    Support preceeding image scaling, using the rotozoomer (from SDL_gfx) from A. Schiffler. Syntax: When loading a new image into cache, one can add an aditional parameter double scale_factor to specify the scaling factor.
    git-svn-id: svn://svn.icculus.org/lost_penguins/trunk@100 e97695b5-5de5-0310-b40a-dc6bdf48eec5

--- src/imgcache.cpp
+++ src/imgcache.cpp
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "SDL_rotozoom.h"
 #include "imgcache.h"
 
 
@@ -6,23 +7,29 @@ ImageCache::ImageCache() {
     not_found="no_file.bmp";
 }
 ImageCache::~ImageCache() {
-    map<string, SDL_Surface*>::iterator imgit=imgcache.begin();
+    map<pair<string,double>, SDL_Surface*>::iterator imgit=imgcache.begin();
     while (imgit != imgcache.end()) {
         SDL_FreeSurface((*imgit).second);
         imgcache.erase(imgit++);
     }
 }
 
-SDL_Surface* ImageCache::loadImage(string imagename) {
+//TODO: maybe replace double scale_factor by string scale_format or sthg like that
+SDL_Surface* ImageCache::loadImage(string imagename, double scale_factor) {
     //default is not_found
-    if (imagename=="") imagename=not_found;
-    map<string, SDL_Surface*>::iterator imgit=imgcache.find(imagename);
+    if (imagename=="") {
+        imagename=not_found;
+        scale_factor=1;
+    }
+        
+    map<pair<string,double>, SDL_Surface*>::iterator imgit=imgcache.find(make_pair(imagename,scale_factor));
     
-    //load new image
+    // Image is not yet cached
     if (imgit==imgcache.end()) {
         string loadfile=config.datadir+imagename;
         SDL_Surface* tmpimg=NULL;
         SDL_Surface* returnimg=NULL;
+        // Loading
         #ifdef SDL_IMAGE
         if ((tmpimg=IMG_Load(loadfile.c_str())) == NULL) {
         #else
@@ -36,23 +43,45 @@ SDL_Surface* ImageCache::loadImage(string imagename) {
             } else {
                 return loadImage(not_found);
             }
+        // Preprocessing
         } else {
+            // About ColorKeys: I'm not sure anymore what is necessary and what not, as some stuff is now
+            // also done in SDL_rotozoom.cpp, at least it seems to work this way...
+            // Image's colorkey have to be set before (if unset) as the SCALING changes the color values
+            #ifdef ALPHA
+            if (!(tmpimg->flags & (SDL_SRCCOLORKEY|SDL_SRCALPHA))) {
+            #else
+            if (!(tmpimg->flags & (SDL_SRCCOLORKEY))) {
+            #endif
+                SDL_SetColorKey(tmpimg, SDL_SRCCOLORKEY|SDL_RLEACCEL, SDL_MapRGB(tmpimg->format,255,0,255));
+            }
+            // SCALING
+            if (scale_factor!=1) tmpimg=scaleImage(tmpimg,scale_factor);
+            // Image's colorkey
             if (tmpimg->flags & SDL_SRCCOLORKEY) {
                 SDL_SetColorKey(tmpimg, SDL_SRCCOLORKEY|SDL_RLEACCEL, tmpimg->format->colorkey);
                 returnimg=SDL_DisplayFormat(tmpimg);
             #ifdef ALPHA
+            // Alpha
             } else if (tmpimg->flags & SDL_SRCALPHA) {
                 SDL_SetAlpha(tmpimg, SDL_SRCALPHA|SDL_RLEACCEL, tmpimg->format->alpha);
                 returnimg=SDL_DisplayFormatAlpha(tmpimg);
             #endif
+            // Our own colorkey (0xff00ff)
             } else {
                 SDL_SetColorKey(tmpimg, SDL_SRCCOLORKEY|SDL_RLEACCEL, SDL_MapRGB(tmpimg->format,255,0,255));
                 returnimg=SDL_DisplayFormat(tmpimg);
             }
             SDL_FreeSurface(tmpimg);
         }
-        imgcache.insert(make_pair(imagename,returnimg));
+        imgcache.insert(make_pair(make_pair(imagename,scale_factor),returnimg));
         return returnimg;
-    //already there
+    // Return the existing Image in cache...
     } else return (*imgit).second;
+}
+
+inline SDL_Surface* ImageCache::scaleImage(SDL_Surface* original_image, double scale_factor) {
+    SDL_Surface* scaled_image=zoomSurface(original_image, scale_factor, scale_factor, 1);
+    SDL_FreeSurface(original_image);
+    return scaled_image;
 }
--- src/imgcache.h
+++ src/imgcache.h
@@ -13,10 +13,12 @@ class ImageCache {
         /// Loads an image if it isn't already contained in the pool
         /// according to it's image name. If the image is not found
         /// it tries to load a fallback image (not_found).
-        SDL_Surface* loadImage(string imagename="");
+        SDL_Surface* loadImage(string imagename="", double scale_factor=1);
     private:
-        std::map<string,SDL_Surface*> imgcache;
+        std::map<pair<string,double>,SDL_Surface*> imgcache;
         string not_found;
+    private:
+        inline SDL_Surface* scaleImage(SDL_Surface* original_image, double scale_factor);
 };
 
 #endif
